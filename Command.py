9245f9aad6c0873c9a7c968f4cc91f36cd3a6f65
Anders Jansson <andersjansson1994@gmail.com>
2021-11-12 11:35:33 +0100

    Added connection to web server

--- Command.py
+++ Command.py
@@ -1,20 +1,31 @@
 # Model class of a command
+
 class Command:
     # Base case of a command if none of the conditions below is triggered
     command = "Unknown error"
+
     # Constructor
-    def __init__(self, blinks):
-        # If blinked less then 3 times
-        if(blinks < 3):
-            self.command = "On/Off"
+    def __init__(self):
+        self.statusOn = False
 
-        # If blinked between 3 and 4 times
-        elif(blinks >= 3 and blinks < 5):
-            self.command = "Channel up"
-        # If blinked between 5 and 6 times
-        elif(blinks >= 5 and blinks < 7):
-            self.command = "Channel down"
-        # If blinked 7 or more times
-        elif(blinks >= 7):
-            self.command = "SOS"
 
+    def updateCommand(self, blinks):
+        if blinks == 1:
+            if self.statusOn:
+                self.command = "{'_id':'Livingroom TV', 'on':'false'}"
+                self.statusOn = False
+            else:
+                self.command = "{'_id':'Livingroom TV', 'on':'true'}"
+                self.statusOn = True
+
+            # If blinked between 3 and 4 times
+        elif blinks == 2:
+            self.command = "{'_id':'Livingroom TV', 'channel':'3'}"
+            # If blinked between 5 and 6 times
+        elif blinks == 3:
+            self.command = "{'_id':'Livingroom TV', 'channel':'4'}"
+            # If blinked 7 or more times
+        elif blinks == 4:
+            self.command = "{'_id':'Livingroom TV', 'channel':'5'}"
+        elif blinks == 5:
+            self.command = "{'_id':'Livingroom TV', 'channel':'6'}"
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import socket
 import time
+from websocket import create_connection
 
 from Command import Command
 
 PORT = 13854
 HOST = 'localhost'
+stateOn = False
+
+ws = create_connection("ws://localhost:1337/websocket")
+command = Command()
 
 # Simple socket client for collecting data from Mindwave mobile headset
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -14,14 +19,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         blinks = 0
         startTime = time.time()
-        # Loops when there are less then 2 seconds between blinks
+    # Loops when there are less then 2 seconds between blinks
         while (time.time() - startTime) <= 2 or blinks == 0:
-            # Receive data from headset
+        # Receive data from headset
             data = s.recv(1024)
-            # Decode as a string
+        # Decode as a string
             stringRep = data.decode('utf-8')
 
-            # Check if a blink has occurred, if so increment blinks and reset timer.
+        # Check if a blink has occurred, if so increment blinks and reset timer.
             if stringRep.__contains__("blinkStrength"):
                 print("registered blink")
                 blinks += 1
@@ -29,7 +34,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             #     if blinks is still 0, reset timer.
             elif blinks == 0:
                 startTime = time.time()
-        # Create a command out of the number of blinks
-        command = Command(blinks)
-        print(f"Command: {command.command}")
-#         TODO: Send command to web server
+    # Create a command out of the number of blinks
+        command.updateCommand(blinks)
+        ws = create_connection("ws://localhost:1337/websocket")
+        ws.send(f"changeDeviceStatus={command.command}")
+        result = ws.recv()
+        print(result)
+        ws.close()
